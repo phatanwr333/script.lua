32387beb0ba04739a7d25f1ad057d226bb1aa904
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2025-02-21 10:19:53 +0700

    Update OwnerTools.lua

--- Mics/OwnerTools.lua
+++ Mics/OwnerTools.lua
@@ -1,35 +1,32 @@
-local _ = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))() 
-local __ = _.Window("Owner Tools")
-local ___ = "https://raw.githubusercontent.com/phatanwr333/script.lua/refs/heads/main/Mics/"
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/turtle"))()
+local window = library.Window("Owner Tools")
+local scriptBaseUrl = "https://raw.githubusercontent.com/phatanwr333/script.lua/refs/heads/main/Mics/"
 
-local c = string.char
-local conmemaybeophivcl = c(83, 97, 118, 101, 32, 73, 110, 115, 116, 97, 110, 99, 101)
-local cubamaycong = c(67, 111, 112, 121, 32, 67, 70, 114, 97, 109, 101)
-local chicuamayngonlam = c(73, 110, 102, 105, 110, 105, 116, 101, 32, 89, 105, 101, 108, 100)
-local onggiamaynguvkl = c(70, 108, 121)
-local emgaicuamaymupvcl = c(68, 101, 120, 32, 69, 120, 112, 108, 111, 114, 101)
-local memaybeonhatvietnam = c(82, 101, 109, 111, 116, 101, 83, 112, 121)
-
-__ :Button(conmemaybeophivcl, function() 
-    setclipboard(c(108, 111, 97, 100, 115, 116, 114, 105, 110, 103) .. "(game:HttpGet(\"" .. "https://raw.githubusercontent.com/phatanwr333/script.lua/refs/heads/main/Mics/SaveInstance.lua" .. "\"))() , how to use: saveinstance(object , fn)")
+window:Button("Save Instance", function()
+    setclipboard("https://raw.githubusercontent.com/phatanwr333/script.lua/refs/heads/main/Mics/ , find SaveIntance.lua")
 end)
 
-__ :Button(cubamaycong, function() 
-    setclipboard('CFrame.new('..tostring(math.floor(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position.x))..','..tostring(math.floor(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position.y))..','..tostring(math.floor(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position.z))..')')
+window:Button("Copy CFrame", function()
+    local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position
+    setclipboard(string.format("CFrame.new(%d, %d, %d)", pos.x, pos.y, pos.z))
 end)
 
-__ :Button(chicuamayngonlam, function() 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() 
+window:Button("Infinite Yield", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end)
 
-__ :Button(onggiamaynguvkl, function() 
-    loadstring(game:HttpGet(___ .. "Fly.lua"))() 
+window:Button("Fly", function()
+    loadstring(game:HttpGet(scriptBaseUrl .. "Fly.lua"))()
 end)
 
-__ :Button(emgaicuamaymupvcl, function() 
-    loadstring(game:HttpGet(___ .. "DexExplorer.lua"))() 
+window:Button("Dex Explorer", function()
+    loadstring(game:HttpGet(scriptBaseUrl .. "DexExplorer.lua"))()
 end)
 
-__ :Button(memaybeonhatvietnam, function() 
-    loadstring(game:HttpGet(___ .. "RemoteSpy.lua"))() 
+window:Button("Remote Spy", function()
+    loadstring(game:HttpGet(scriptBaseUrl .. "RemoteSpy.lua"))()
+end)
+
+window:Button("Turtle Spy", function()
+    loadstring(game:HttpGet(scriptBaseUrl .. "TurtleSpy.lua"))()
 end)
